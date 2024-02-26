5efbf9311cef19a17e75a5743df6be7284fe16fb
hayato-miura <miura.miura.515@gmail.com>
2024-02-26 16:43:12 +0900

    一度エラーが出たのでコミットします

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { InputForm } from './components/InputForm';
+import { Title } from './components/Title';
+import { TodoList } from './components/TodoList';
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="body">
+      <Title />
+      <InputForm />
+      <TodoList />
     </div>
   );
 }
